a0e6cc19e13b95e458fa982287bc3221e2a84431
Leonardo Leite <leolsumadjer@gmail.com>
2018-06-13 23:26:07 -0300

    Mensagem conforme requisição

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+  res.send("Requisição GET");
+});
+
+router.post('/', function(req, res, next){
+  res.send("Requisição POST");
 });
 
 module.exports = router;
